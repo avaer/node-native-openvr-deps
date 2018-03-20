a71517929cfe821255d2fe4416e2510cc3f76777
Avaer Kazmer <a@modules.io>
2018-03-20 04:21:06 -0700

    Update postinstall script to clean non-platform libs

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,57 @@
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 const unzip = require('unzip');
-fs.createReadStream(path.join(__dirname, 'lib.zip'))
+const rimraf = require('rimraf');
+const ws = fs.createReadStream(path.join(__dirname, 'lib.zip'))
   .pipe(unzip.Extract({
     path: __dirname,
   }));
+
+ws.on('close', () => {
+  rimraf(path.join(__dirname, 'lib.zip'), err => {
+    if (err) {
+      throw err;
+    }
+  });
+  const platform = os.platform();
+  switch (platform) {
+    case 'win32': {
+      ['bin', 'lib'].forEach(d => {
+        ['osx32', 'osx64', 'linux32', 'linux64'].forEach(p => {
+          rimraf(path.join(__dirname, d, p), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+      });
+      break;
+    }
+    case 'darwin': {
+      ['bin', 'lib'].forEach(d => {
+        ['linux32', 'linux64', 'win32', 'win64'].forEach(p => {
+          rimraf(path.join(__dirname, d, p), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+      });
+      break;
+    }
+    case 'linux': {
+      ['bin', 'lib'].forEach(d => {
+        ['win32', 'win64', 'osx32', 'osx64'].forEach(p => {
+          rimraf(path.join(__dirname, d, p), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+      });
+      break;
+    }
+    default: throw new Error('unknown platform: ' + platform);
+  }
+});
